b8562e47323127cff178641a344042eb8e4671cd
mariapaula1108 <mapaula@bu.edu>
2022-09-16 18:23:58 -0400

    Initial import

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "./ignorewarnings";
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import HomeScreen from "./screens/HomeScreen";
+import UserScreen from "./screens/UserScreen";
+import SettingScreen from "./screens/SettingScreen.js";
+
+const AppNavigator = createStackNavigator(
+{
+	Home: HomeScreen,
+	User: UserScreen,
+	Setting: SettingScreen,
+},
+{
+	defaultNavigationOptions: {
+	headerStyle: {
+		backgroundColor: "#006600",
+	},
+	headerTitleStyle: {
+		fontWeight: "bold",
+		color: "#FFF",
+	},
+	headerTintColor: "#FFF",
+	},
+},
+{
+	initialRouteName: "Home",
+}
+);
+
+const Navigator = createAppContainer(AppNavigator);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+return (
+	<Navigator>
+	<HomeScreen />
+	</Navigator>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
